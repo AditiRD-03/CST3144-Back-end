0e0b7378d6888c9627a48b1858e39c4bad0c6f97
Aditi <aditidevadiga03@gmail.com>
2025-04-08 22:20:36 +0400

    update to server.js

--- server.js
+++ server.js
@@ -14,3 +14,11 @@ app.use((req, res, next) => {
     console.log("Request Date: " + new Date());
     next();
 });
+// CORS headers to allow cross-origin requests 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");  
+    next();  
+});
